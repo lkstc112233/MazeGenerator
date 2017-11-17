166399921e74145aa30054189577473c767c0c24
Photon Cat <lkstc112233@gmail.com>
2017-11-17 00:50:11 -0500

    ........Sad

---
+++ MazeGenerator/MazeGenerator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MazeGenerator
+//
+//  Created by 煦 柯 on 2017/11/17.
+//  Copyright © 2017年 Must Studio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
