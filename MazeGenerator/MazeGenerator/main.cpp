877c89e6d71b6a277c2c45ba8a471a18880df12c
Photon Cat <lkstc112233@gmail.com>
2017-11-17 01:12:01 -0500

    BMP header

--- MazeGenerator/MazeGenerator/main.cpp
+++ MazeGenerator/MazeGenerator/main.cpp
@@ -7,9 +7,58 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <utility>
+#include <vector>
+#include <chrono>
+#include <algorithm>
+#include <string>
+
+class Maze
+{
+public:
+    int width;
+    int height;
+    int pathWidth;
+    int wallWidth;
+    void write(std::string& fileName)
+    {
+        std::ofstream ofs(fileName, std::ios::binary);
+        int fourb;
+        short twob;
+        // BMP header
+        ofs.put('B');
+        ofs.put('M');
+        fourb = getFileSize();
+        ofs.write(reinterpret_cast<char*>(&fourb), 4);
+        fourb = 0;
+        ofs.write(reinterpret_cast<char*>(&fourb), 4);
+        fourb = 54;
+        ofs.write(reinterpret_cast<char*>(&fourb), 4);
+        // DIB header
+        
+    }
+    
+    
+    
+    int getBitmapRowWidth()
+    {
+        return (24 * width + 31) / 32 * 4;
+    }
+    
+    int getFileSize()
+    {
+        return getBitmapSize() + 54;
+    }
+    int getBitmapSize()
+    {
+        return height * getBitmapRowWidth();
+    }
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    
+    
     std::cout << "Hello, World!\n";
     return 0;
 }
